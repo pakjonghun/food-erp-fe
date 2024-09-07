5badbcea01c457eb8ba84f7ebc3bd8e73041dff5
pakjonghun <150112208+pakpakjonghun@users.noreply.github.com>
2024-09-07 20:13:45 +0900

    루트 페이지 작성

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { AUTH_TOKEN } from '@/constants/global';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+const Page = () => {
+  const cookie = cookies().get(AUTH_TOKEN);
+  const nextPath = cookie ? '/dashboard' : '/login';
+
+  redirect(nextPath);
+};
+
+export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,24 +4,23 @@ import { auth } from './actions/auth';
 import { publicPathList } from './constants/route';
 
 export async function middleware(request: NextRequest) {
-  console.log(1);
   const path = request.nextUrl.pathname;
   const isPublic = publicPathList.some((p) => path.indexOf(p) != -1);
   const cookie = request.cookies.get(AUTH_TOKEN);
-  // if (!cookie && !isPublic) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (!cookie && !isPublic) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-  // const isGranted = await auth(cookie?.value ?? '');
+  const isGranted = await auth(cookie?.value ?? '');
 
-  // if (isPublic && isGranted) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // }
+  if (isPublic && isGranted) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
-  // if (!isPublic && !isGranted) {
-  //   request.cookies.delete(AUTH_TOKEN);
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (!isPublic && !isGranted) {
+    request.cookies.delete(AUTH_TOKEN);
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 }
 
 export const config = {
