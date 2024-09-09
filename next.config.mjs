4688f5582bc157314988cad203729af82f46485c
jonghunpak <fireking5997@gmail.co.kr>
2024-09-09 22:48:03 +0900

    이미지 최적화 옵션 제거; g

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'd3kouwd62bvdx0.cloudfront.net',
-        port: '',
-        pathname: '/**', // 모든 하위 경로 허용
-      },
-      {
-        protocol: 'https',
-        hostname: 'best5997.com',
-        port: '',
-        pathname: '/**', // 모든 하위 경로 허용
-      },
-    ],
     unoptimized: true,
   },
 };
