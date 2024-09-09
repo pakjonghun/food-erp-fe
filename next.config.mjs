61e158a0a198488d788f39d29b62bf62509c1fc8
jonghunpak <fireking5997@gmail.co.kr>
2024-09-09 21:44:55 +0900

    허용된 경로 1개 더 추가해서 이미지 최적화 서용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'best5997.com', // CloudFront 도메인 또는 외부 도메인
+        port: '',
+        pathname: '/**', // 모든 하위 경로 허용
+      },
+    ],
+  },
+};
 
 export default nextConfig;
