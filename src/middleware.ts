d7e73ff884e0335c7710fa88b0395412691f9216
jonghunpak <fireking5997@gmail.co.kr>
2024-09-11 12:12:47 +0900

    에셋 폴더 미들웨어에서 확인안하도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,5 +27,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+  matcher: ['/((?!api|_next/static|assets|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
