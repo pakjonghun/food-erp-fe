3634dcd5c4a9a88221a9c99a6fd63e0702940d34
jonghunpak <fireking5997@gmail.co.kr>
2024-09-11 11:10:31 +0900

    image config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: process.env.NEXT_PUBLIC_CLOUD_DOMAIN }],
+  },
+};
 
 export default nextConfig;
